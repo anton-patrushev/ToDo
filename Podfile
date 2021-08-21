24ae5f1fd04223ddda845fd1cc62969578865e3f
anton-patrushev <anton11patrushev@gmail.com>
2021-08-21 15:07:43 +0300

    Remove StoryBoard usage and initialize project structure

--- Podfile
+++ Podfile
@@ -1,10 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'ToDoApplication' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for ToDoApplication
+  pod 'RxSwift', '6.2.0'
+  pod 'RxCocoa', '6.2.0'
+  pod 'SnapKit', '~> 5.0.0'
 
 end
---
+++ ToDoApplication/Source/Views/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  ToDoApplication
+//
+//  Created by Anton Patrushev on 21.08.21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
